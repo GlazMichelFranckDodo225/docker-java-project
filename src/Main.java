27aa51186558e8d27ae782aafc455c4e57230e25
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-05 06:52:27 +0200

    Implementation of the Project to Dockerize

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Java");
+        printSystemProperties();
     }
+
+    public static void printSystemProperties() {
+        System.out.println("The Printing Properties");
+        Properties props = System.getProperties();
+        System.out.println(props);
+    }
+
 }
